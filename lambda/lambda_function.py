f39f67ab6cb6be0a5cf076d49c5ebb077cf5ce24
Luke Lopez <xlucas14@gmail.com>
2020-03-18 22:55:14 -0400

    Requests must have token now, removed dead code

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,12 +1,8 @@
 # -*- coding: utf-8 -*-
-
-# This sample demonstrates handling intents from an Alexa skill using the Alexa Skills Kit SDK for Python.
-# Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
-# session persistence, api calls, and more.
-# This sample is built using the handler classes approach in skill builder.
 import requests
 import logging
 import json
+import os
 
 import ask_sdk_core.utils as ask_utils
 
@@ -17,25 +13,25 @@ from ask_sdk_core.handler_input import HandlerInput
 
 from ask_sdk_model import Response
 
+from setup import API_KEY
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
 class LaunchRequestHandler(AbstractRequestHandler):
     """Handler for Skill Launch."""
-    def can_handle(self, handler_input):
-        # type: (HandlerInput) -> bool
 
+    def can_handle(self, handler_input):
         return ask_utils.is_request_type("LaunchRequest")(handler_input)
 
     def handle(self, handler_input):
-        # type: (HandlerInput) -> Response
         speak_output = "Welcome, you can say Hello or Help. Which would you like to try?"
         response = (handler_input.response_builder
-                .speak(speak_output)
-                .ask(speak_output)
-                .response)
-        
+                    .speak(speak_output)
+                    .ask(speak_output)
+                    .response)
+
         logger.info(response)
         return (
             response
@@ -44,59 +40,55 @@ class LaunchRequestHandler(AbstractRequestHandler):
 
 class BloodSugarIntentHandler(AbstractRequestHandler):
     """Handler for Blood Sugar Intent."""
+
     def can_handle(self, handler_input):
-        logger.debug("can_handle")
-        # type: (HandlerInput) -> bool
         return ask_utils.is_intent_name("BloodSugarIntent")(handler_input)
 
     def handle(self, handler_input):
-        # type: (HandlerInput) -> Response
-
         base_url = "http://luke-lopez-cgm.herokuapp.com"
+        token = "?token=" + API_KEY
         endpoint = "/api/v1/entries/sgv.json"
-        directions = {"Flat": "steady"}
-        
-        res = requests.get(base_url + endpoint)
-        
+
+        directions = {
+            "Flat": "steady",
+            "FortyFiveUp": "rising",
+            "FortyFiveDown": "falling",
+            "SingleUp": "single up",
+            "DoubleUp": "double up",
+            "SingleDown": "single down",
+            "DoubleDown": "double down",
+            "NONE": "no slope",
+            "NOT_COMPUTABLE": "the slope is not computable",
+            "RATE_OUT_OF_RANGE": "the rate is out of range"
+        }
+
+        res = requests.get(base_url + endpoint + token)
+
         if res.status_code == 200:
             data = json.loads(res.content)
             blood_sugar = str(data[0]["sgv"])
-            direction = directions[data[0]["direction"]]
-            
+            try:
+                direction = directions[data[0]["direction"]]
+            except KeyError:
+                direction = "unable to get direction data."
+
             input_subject = handler_input.request_envelope.request.intent.slots["subject"].value
             subject = "your" if input_subject == "my" else "luke's"
-            
-            speak_output = f"{subject} blood sugar is {blood_sugar}, {direction}."
+
+            speak_output = f"{subject} blood sugar is {blood_sugar} and {direction}."
         else:
             speak_output = "I couldn't get the reading. Please try again!"
 
         return (
             handler_input.response_builder
-                .speak(speak_output)
-                .response
-        )
-
-
-class HelpIntentHandler(AbstractRequestHandler):
-    """Handler for Help Intent."""
-    def can_handle(self, handler_input):
-        # type: (HandlerInput) -> bool
-        return ask_utils.is_intent_name("AMAZON.HelpIntent")(handler_input)
-
-    def handle(self, handler_input):
-        # type: (HandlerInput) -> Response
-        speak_output = "You can say hello to me! How can I help?"
-
-        return (
-            handler_input.response_builder
-                .speak(speak_output)
-                .ask(speak_output)
-                .response
+            .speak(speak_output)
+            .response
         )
 
 
 class CancelOrStopIntentHandler(AbstractRequestHandler):
     """Single handler for Cancel and Stop Intent."""
+
     def can_handle(self, handler_input):
         # type: (HandlerInput) -> bool
         return (ask_utils.is_intent_name("AMAZON.CancelIntent")(handler_input) or
@@ -108,22 +100,18 @@ class CancelOrStopIntentHandler(AbstractRequestHandler):
 
         return (
             handler_input.response_builder
-                .speak(speak_output)
-                .response
+            .speak(speak_output)
+            .response
         )
 
 
 class SessionEndedRequestHandler(AbstractRequestHandler):
     """Handler for Session End."""
+
     def can_handle(self, handler_input):
-        # type: (HandlerInput) -> bool
         return ask_utils.is_request_type("SessionEndedRequest")(handler_input)
 
     def handle(self, handler_input):
-        # type: (HandlerInput) -> Response
-
-        # Any cleanup logic goes here.
-
         return handler_input.response_builder.response
 
 
@@ -133,20 +121,18 @@ class IntentReflectorHandler(AbstractRequestHandler):
     for your intents by defining them above, then also adding them to the request
     handler chain below.
     """
+
     def can_handle(self, handler_input):
-        # type: (HandlerInput) -> bool
         return ask_utils.is_request_type("IntentRequest")(handler_input)
 
     def handle(self, handler_input):
-        # type: (HandlerInput) -> Response
         intent_name = ask_utils.get_intent_name(handler_input)
         speak_output = "You just triggered " + intent_name + "."
 
         return (
             handler_input.response_builder
-                .speak(speak_output)
-                # .ask("add a reprompt if you want to keep the session open for the user to respond")
-                .response
+            .speak(speak_output)
+            .response
         )
 
 
@@ -155,22 +141,22 @@ class CatchAllExceptionHandler(AbstractExceptionHandler):
     stating the request handler chain is not found, you have not implemented a handler for
     the intent being invoked or included it in the skill builder below.
     """
+
     def can_handle(self, handler_input, exception):
-        # type: (HandlerInput, Exception) -> bool
         return True
 
     def handle(self, handler_input, exception):
-        # type: (HandlerInput, Exception) -> Response
         logger.error(exception, exc_info=True)
 
         speak_output = "Sorry, I had trouble doing what you asked. Please try again."
 
         return (
             handler_input.response_builder
-                .speak(speak_output)
-                .ask(speak_output)
-                .response
+            .speak(speak_output)
+            .ask(speak_output)
+            .response
         )
+
 
 # The SkillBuilder object acts as the entry point for your skill, routing all request and response
 # payloads to the handlers above. Make sure any new handlers or interceptors you've
@@ -181,10 +167,10 @@ sb = SkillBuilder()
 
 sb.add_request_handler(LaunchRequestHandler())
 sb.add_request_handler(BloodSugarIntentHandler())
-sb.add_request_handler(HelpIntentHandler())
 sb.add_request_handler(CancelOrStopIntentHandler())
 sb.add_request_handler(SessionEndedRequestHandler())
-sb.add_request_handler(IntentReflectorHandler()) # make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
+# make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
+sb.add_request_handler(IntentReflectorHandler())
 
 sb.add_exception_handler(CatchAllExceptionHandler())
 
